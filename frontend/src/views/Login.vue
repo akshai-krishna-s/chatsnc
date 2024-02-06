721106a4c284773b8e3b20b407440f65aed8884d
Akshai Krishna S <akshai404akz@gmail.com>
2024-02-06 14:19:13 +0530

    Update Login.vue styling and font sizes

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -4,16 +4,16 @@
       <div class="relative h-full">
         <div class="flex h-full flex-col items-center justify-center">
           <form class="flex flex-col gap-3 bg-gray-medium items-center px-10 py-8 rounded-md">
-            <p class="text-center text-xl font-medium mb-3">Login to ChatSNC</p>
+            <p class="text-center text-2xl font-medium mb-3">Login to ChatSNC</p>
             <div class="flex flex-col gap-1">
-              <label for="email" class="text-sm">Email</label>
-              <input type="email" id="email" class="bg-transparent border rounded-md w-56" />
+              <label for="email" class="text-base">Email</label>
+              <input type="email" id="email" class="bg-transparent border h-8 rounded-md w-60" />
             </div>
             <div class="flex flex-col gap-1">
-              <label for="password" class="text-sm">Password</label>
-              <input type="password" id="password" class="bg-transparent border rounded-md w-56" />
+              <label for="password" class="text-base">Password</label>
+              <input type="password" id="password" class="bg-transparent border h-8 rounded-md w-60" />
             </div>
-            <button class="text-gray-medium bg-gray-light w-fit px-2 font-medium rounded-md">Login</button>
+            <button class="text-gray-medium text-base bg-gray-light w-fit px-4 py-1 font-medium rounded-md">Login</button>
             <p class="text-sm">Don't have an account? <router-link class="text-blue-primary" to="/register">Register</router-link></p>
           </form>
         </div>

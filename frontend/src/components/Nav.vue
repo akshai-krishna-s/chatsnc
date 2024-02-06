86938b9073ee819364299baa0080fb3984320309
Akshai Krishna S <akshai404akz@gmail.com>
2024-02-06 22:59:59 +0530

    Update Nav and Login components

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-full p-12 flex sticky top-0 z-10 min-h-[40px] shadow-md shadow-shadow py-4 justify-between items-center">
+  <div class="w-full px-8 md:px-12 flex sticky top-0 z-10 min-h-[40px] shadow-md shadow-shadow py-3 justify-between items-center">
     <p class="text-xl font-bold">ChatSNC</p>
     <div v-if="isUserInHomePage()" class="cursor-pointer" @click="logoutUser">
-      <svg width="42" height="42" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg width="36" height="36" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="25" cy="25" r="24" stroke="#DEDEDE" stroke-width="2" />
         <path
           d="M15 37V12H27.5V14.7778H17.7778V34.2222H27.5V37H15ZM33.0556 31.4444L31.1458 29.4306L34.6875 25.8889H23.3333V23.1111H34.6875L31.1458 19.5694L33.0556 17.5556L40 24.5L33.0556 31.4444Z"
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,7 +3,7 @@
     <div class="flex-1 overflow-hidden">
       <div class="relative h-full">
         <div class="flex h-full flex-col items-center justify-center">
-          <form class="flex flex-col gap-3 bg-gray-medium items-center px-10 py-8 rounded-md" @submit.prevent="loginUser">
+          <form class="flex flex-col gap-3 mt- bg-gray-medium items-center px-10 py-8 rounded-md" @submit.prevent="loginUser">
             <p class="text-center text-2xl font-medium mb-3">Login to ChatSNC</p>
             <div class="flex flex-col gap-1">
               <label for="email" class="text-base">Email</label>
@@ -13,7 +13,15 @@
               <label for="password" class="text-base">Password</label>
               <input type="password" id="password" name="password" class="bg-transparent border h-8 rounded-md w-60 pl-2 focus:outline-none focus:ring-1 ring-slate-300" v-model="data.password" />
             </div>
-            <button class="text-gray-medium text-base bg-gray-light w-fit px-4 py-1 font-medium rounded-md">Login</button>
+            <button class="text-gray-medium flex justify-center text-base bg-gray-light w-fit px-4 py-1 font-medium rounded-md">
+              <p v-if="!loading">Login</p>
+              <div v-else>
+                <svg class="w-6 h-6 text-gray-medium animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle class="opacity-10" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                  <path fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+              </div>
+            </button>
             <p v-if="error" class="text-red-500 text-sm">{{ error }}</p>
             <p class="text-sm">Don't have an account? <router-link class="text-blue-primary" to="/register">Register</router-link></p>
           </form>
@@ -35,10 +43,13 @@ const data = ref({
   password: '',
 })
 
+const loading = ref(false)
+
 const error = ref('')
 
 async function loginUser() {
   try {
+    loading.value = true
     const response = await axios.post(
       'login',
       {
@@ -62,6 +73,8 @@ async function loginUser() {
     if (e.response.status === 422) {
       error.value = 'Enter a valid email and password'
     }
+  } finally {
+    loading.value = false
   }
 }
 </script>

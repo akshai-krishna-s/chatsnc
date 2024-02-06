7de685837c5525c0ea862cd72bc49c41681a12ea
Akshai Krishna S <akshai404akz@gmail.com>
2024-02-06 12:20:28 +0530

    Refactor chat input styling

--- frontend/src/views/ChatSNC.vue
+++ frontend/src/views/ChatSNC.vue
@@ -9,7 +9,7 @@
       </div> -->
       <div class="relative h-full">
         <div class="h-full w-full overflow-y-auto">
-          <div class="flex flex-col">
+          <div class="flex flex-col md:w-3/6 md:mx-auto px-4 md:px-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit optio atque repellat debitis accusantium minima aut itaque quo quas perferendis, tempora, quidem possimus similique.
             Labore velit incidunt omnis dolore qui. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, veniam. Tempore architecto ab optio quia alias repellendus. Aut nulla nam odit?
             Autem, nam alias sed consectetur assumenda repellendus aut provident! Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit optio atque repellat debitis accusantium minima aut
@@ -47,14 +47,21 @@
       <form class="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div class="relative flex h-full flex-1 items-stretch md:flex-col">
           <div class="flex w-full items-center">
-            <div class="overflow-hidden flex flex-col w-full flex-grow relative border rounded-2xl">
-              <textarea
-                tabindex="0"
-                rows="1"
+            <div
+              class="overflow-hidden flex w-full flex-grow relative border rounded-full border-gray items-center pl-4 pr-1 gap-2 [&:has(input:focus)]:border-gray-light [&:has(input:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)]"
+            >
+              <input
                 placeholder="Type your message here..."
-                class="m-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 md:py-3.5 md:pr-12 pl-3 md:pl-4"
-                style="max-height: 200px; height: 44px; overflow-y: hidden"
-              ></textarea>
+                class="w-full h-7 resize-none border-0 bg-transparent focus:ring-offset-0 focus:outline-none placeholder:font-light placeholder-gray-light focus:ring-0 focus-visible:ring-0"
+                style="overflow-y: hidden"
+              />
+              <span>
+                <svg width="55" height="55" viewBox="0 0 60 59" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="30.1875" cy="29.5" r="29.375" fill="#282A36" />
+                  <path d="M30.1875 20.3203V38.6797" stroke="#C3CADB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  <path d="M21.0078 29.5H39.3672" stroke="#C3CADB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </span>
             </div>
           </div>
         </div>

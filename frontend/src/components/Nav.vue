36ccc00117ea864df212fe33e3a15207fc348f88
Akshai Krishna S <akshai404akz@gmail.com>
2024-02-07 10:57:37 +0530

    Add font preconnect and update colors in index.html and Nav.vue components

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -3,10 +3,10 @@
     <p class="text-xl font-bold">ChatSNC</p>
     <div v-if="isUserInHomePage()" class="cursor-pointer" @click="logoutUser">
       <svg width="36" height="36" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="25" cy="25" r="24" stroke="#DEDEDE" stroke-width="2" />
+        <circle cx="25" cy="25" r="24" stroke="#C3CADB" stroke-width="2" />
         <path
           d="M15 37V12H27.5V14.7778H17.7778V34.2222H27.5V37H15ZM33.0556 31.4444L31.1458 29.4306L34.6875 25.8889H23.3333V23.1111H34.6875L31.1458 19.5694L33.0556 17.5556L40 24.5L33.0556 31.4444Z"
-          fill="#DEDEDE"
+          fill="#C3CADB"
         />
       </svg>
     </div>
--- frontend/src/views/ChatSNC.vue
+++ frontend/src/views/ChatSNC.vue
@@ -81,11 +81,26 @@
                 class="w-full h-7 resize-none border-0 bg-transparent focus:ring-offset-0 focus:outline-none placeholder:font-light placeholder-gray-light focus:ring-0 focus-visible:ring-0"
                 style="overflow-y: hidden"
               />
-              <span>
-                <svg width="50" height="50" viewBox="0 0 60 59" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <circle cx="30.1875" cy="29.5" r="29.375" fill="#282A36" />
-                  <path d="M30.1875 20.3203V38.6797" stroke="#C3CADB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                  <path d="M21.0078 29.5H39.3672" stroke="#C3CADB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              <span class="cursor-pointer">
+                <svg width="50" height="50" viewBox="0 0 66 67" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g filter="url(#filter0_d_29_32)">
+                    <circle cx="33.1875" cy="33.5" r="29.375" fill="#282A36" />
+                  </g>
+                  <path d="M33.1875 24.3203V42.6797" stroke="#C3CADB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  <path d="M24.0078 33.5H42.3672" stroke="#C3CADB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  <defs>
+                    <filter id="filter0_d_29_32" x="0.6125" y="0.925" width="65.15" height="65.15" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                      <feMorphology radius="1" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_29_32" />
+                      <feOffset />
+                      <feGaussianBlur stdDeviation="1.1" />
+                      <feComposite in2="hardAlpha" operator="out" />
+                      <feColorMatrix type="matrix" values="0 0 0 0 0.76328 0 0 0 0 0.790296 0 0 0 0 0.859766 0 0 0 0.37 0" />
+                      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_29_32" />
+                      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_29_32" result="shape" />
+                    </filter>
+                  </defs>
                 </svg>
               </span>
             </div>

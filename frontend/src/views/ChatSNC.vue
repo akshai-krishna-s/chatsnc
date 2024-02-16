e987ecbe4dc92da64f33dc8614c2efb8544f7163
Akshai Krishna S <akshai404akz@gmail.com>
2024-02-17 00:10:37 +0530

    Update chat input styling and add send message button

--- frontend/src/views/ChatSNC.vue
+++ frontend/src/views/ChatSNC.vue
@@ -25,7 +25,7 @@
         <div class="relative flex h-full flex-1 items-stretch md:flex-col">
           <div class="flex w-full items-center">
             <div
-              class="overflow-hidden flex w-full flex-grow relative border rounded-full border-gray items-center pr-4 gap-2 [&:has(input:focus)]:border-gray-light [&:has(input:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)]"
+              class="overflow-hidden flex w-full flex-grow relative border rounded-full border-gray items-center gap-2 [&:has(input:focus)]:border-gray-light [&:has(input:focus)]:shadow-[0_2px_6px_rgba(0,0,0,.05)]"
             >
               <span class="cursor-pointer" @click="chat = []">
                 <svg width="50" height="50" viewBox="0 0 66 67" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -55,6 +55,30 @@
                 style="overflow-y: hidden"
                 v-model="message"
               />
+              <span class="cursor-pointer" @click="sendMessage">
+                <svg width="50" height="50" viewBox="0 0 66 67" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g filter="url(#filter0_d_46_17)">
+                    <circle cx="32.9531" cy="33.5" r="29.375" fill="#282A36" />
+                  </g>
+                  <path
+                    d="M46.8221 32.918L24.4553 22.2529C24.2301 22.145 23.9792 22.1041 23.732 22.1351C23.4847 22.166 23.2512 22.2675 23.0587 22.4278C22.8662 22.588 22.7227 22.8004 22.6447 23.0402C22.5668 23.28 22.5577 23.5372 22.6186 23.782L25.1697 34.1245L22.6186 44.4669C22.5572 44.7118 22.5658 44.9693 22.6436 45.2094C22.7213 45.4494 22.8649 45.6621 23.0576 45.8225C23.2503 45.9829 23.484 46.0844 23.7316 46.1151C23.9791 46.1458 24.2301 46.1045 24.4553 45.996L46.8221 35.3309C47.0482 35.2233 47.2393 35.0527 47.3732 34.839C47.507 34.6254 47.5781 34.3775 47.5781 34.1245C47.5781 33.8714 47.507 33.6235 47.3732 33.4099C47.2393 33.1963 47.0482 33.0256 46.8221 32.918ZM25.8433 42.3859L26.9472 37.9105L34.4204 34.1245L26.9472 30.3384L25.8433 25.8631L43.1711 34.1245L25.8433 42.3859Z"
+                    fill="#C3CADB"
+                  />
+                  <defs>
+                    <filter id="filter0_d_46_17" x="0.378125" y="0.925" width="65.15" height="65.15" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                      <feMorphology radius="1" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_46_17" />
+                      <feOffset />
+                      <feGaussianBlur stdDeviation="1.1" />
+                      <feComposite in2="hardAlpha" operator="out" />
+                      <feColorMatrix type="matrix" values="0 0 0 0 0.76328 0 0 0 0 0.790296 0 0 0 0 0.859766 0 0 0 0.37 0" />
+                      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_46_17" />
+                      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_46_17" result="shape" />
+                    </filter>
+                  </defs>
+                </svg>
+              </span>
             </div>
           </div>
         </div>

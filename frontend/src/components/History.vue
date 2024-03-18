1f217661b0b172e073debd866efad55944ad1ec6
Akshai Krishna S <akshai404akz@gmail.com>
2024-03-18 17:16:58 +0530

    Refactor History.vue component styling and add loading spinner

--- frontend/src/components/History.vue
+++ frontend/src/components/History.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="w-full md:w-4/12 hidden md:flex self-start text-gray-light border border-gray mt-8 rounded-md shadow-lg py-3 px-4">
-    <div class="flex w-full flex-col gap-1">
-      <h1 class="text-lg font-bold">Recent Chats</h1>
+  <div class="w-full md:w-4/12 hidden md:flex self-start text-gray-light border border-gray mt-8 rounded-md shadow-lg py-4 px-4">
+    <div class="flex w-full flex-col gap-2">
+      <h1 class="text-lg font-bold ml-2">Recent Chats</h1>
       <div v-if="loading" class="flex w-full justify-center items-center">
         <div class="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-light"></div>
       </div>
-      <div class="flex w-full flex-col">
+      <div class="flex w-full gap-2 flex-col">
         <div
           v-for="(item, index) in history.slice(0, 10)"
-          class="flex w-full cursor-pointer py-2 pl-1 hover:bg-gray-medium transition flex-col gap-2 border-b border-gray hover:border-l-4 hover:border-orange-300"
+          class="flex w-full cursor-pointer py-2 px-3 hover:bg-gray-medium transition flex-col gap-2 border-b rounded-md bg-gray-medium border-gray hover:border-l-4 hover:border-orange-300"
           :key="index"
           @click="$emit('getChat', item.id)"
         >

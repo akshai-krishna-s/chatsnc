f357463bd42d9aaad8498b5b59268b68ffe1a2d7
Akshai Krishna S <akshai404akz@gmail.com>
2024-03-25 18:06:34 +0530

    Update database and file names, add new chat data, and refactor code

--- backend/app/routers/chatsnc.py
+++ backend/app/routers/chatsnc.py
@@ -37,6 +37,10 @@ def generate_chat(
     user: schemas.UserOut = Depends(oauth2.get_current_user),
 ):
     chat_history = [
+        # ChatMessage(
+        #     role=MessageRole.SYSTEM,
+        #     content="Please only answer questions if the answer is certain based on the given context. Do not answer if unsure. For inquiries about the current principal or similar topics, answer only if sure. The current principal of SN College is Dr.S.V.Manoj. Avoid answering current affairs questions without certainty.",
+        # ),
         ChatMessage(
             role=MessageRole.USER,
             content="Hello assistant, we are having a insightful discussion about Rules of SN College today.",

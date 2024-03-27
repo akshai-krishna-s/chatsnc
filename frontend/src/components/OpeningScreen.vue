c9d7a69e34be6e678c959ed82eca60295b6249f1
Akshai Krishna S <akshai404akz@gmail.com>
2024-03-27 17:02:18 +0530

    Remove unnecessary query from OpeningScreen.vue

--- frontend/src/components/OpeningScreen.vue
+++ frontend/src/components/OpeningScreen.vue
@@ -28,7 +28,6 @@ const sampleQueries = [
   'What are the facilities available in the Hostel?',
   'What are the scholarships available for students?',
   'Who are the current principal and manager of SN College?',
-  'Where is Multi Gym located in SN College?',
 ]
 
 const randomQueries = sampleQueries.sort(() => Math.random() - Math.random()).slice(0, 3)
